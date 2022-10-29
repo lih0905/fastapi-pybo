c08b3ad522f16904245a31231591fe2fcf9832e0
Inhwan Lee <lih0905@gmail.com>
2022-10-29 18:22:11 +0900

    Add pagination

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -3,11 +3,24 @@
     import { link } from 'svelte-spa-router'
 
     let question_list = []
+    let size = 10
+    let page = 0
+    let total = 0
+    $: total_page = Math.ceil(total/size)
   
-    fastapi('get', '/api/question/list', {}, (json) => {
-        question_list = json
-    })
-    
+    function get_question_list(_page) {
+        let params = {
+            page: _page,
+            size: size,
+        }
+        fastapi('get', '/api/question/list', params, (json) => {
+            question_list = json.question_list
+            page = _page
+            total = json.total
+        })
+    }
+
+    get_question_list(0)
 </script>
   
 <!-- <ul>
@@ -37,5 +50,48 @@
             {/each}
         </tbody>
     </table>
+    <!-- 페이징 처리 시작 -->
+    <ul class="pagination justify-content-center">
+        <!-- 이전 페이지 -->
+        <li class="page-item {page <= 0 && 'disabled'}">
+            <button class="page-link" on:click="{() => get_question_list(page-1)}">이전</button>
+        </li>
+        <!-- 페이지 번호 -->
+        {#each Array(total_page) as _, loop_page}
+        <!-- 첫번째 페이지는 강제로 표시 -->
+        {#if loop_page === 0}
+            <li class="page-item {loop_page === page && 'active'}">
+                <button class="page-link" on:click="{() => get_question_list(loop_page)}">{loop_page+1}</button>
+            </li>
+        <!-- 마지막 페이지도 강제로 표시 -->
+        {:else if loop_page === total_page-1}
+            <li class="page-item {loop_page === page && 'active'}">
+                <button class="page-link" on:click="{() => get_question_list(loop_page)}">{loop_page+1}</button>
+            </li>
+        <!-- 첫번째/마지막 페이지가 아닌 경우, 현재 페이지 전후 3개 페이지를 표시 -->
+        {:else if loop_page > 0 && loop_page >= page-3 && loop_page <= page+3 && loop_page < total_page-1}
+            <!-- 현재 페이지가 5번 이상인 경우 1번째 페이지와 현재페이지-3 페이지 사이에 ... 등록 -->
+            {#if loop_page === page-3 && loop_page > 1}
+            <li class="page-item {'disabled'}">
+                <button class="page-link">...</button>
+            </li>
+            {/if}
+            <li class="page-item {loop_page === page && 'active'}">
+                <button class="page-link" on:click="{() => get_question_list(loop_page)}">{loop_page+1}</button>
+            </li>
+            <!-- 마찬가지로 마지막 페이지와 현재 페이지+3 페이지 사이에 ... 입력 -->
+            {#if loop_page === page+3 && loop_page < total_page-2}
+            <li class="page-item {'disabled'}">
+                <button class="page-link">...</button>
+            </li>
+            {/if}        
+        {/if}
+        {/each}
+        <!-- 다음 페이지 -->
+        <li class="page-item {page >= total_page-1 && 'disabled'}">
+            <button class="page-link" on:click="{() => get_question_list(page+1)}">다음</button>
+        </li>
+    </ul>
+    <!-- 페이징 처리 끝 -->
     <a use:link href="/question-create" class="btn btn-primary">질문 등록하기</a>
 </div>
