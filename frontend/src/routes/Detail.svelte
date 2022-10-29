bb4555af325daa7693cc805717340751a744db4b
Inhwan Lee <lih0905@gmail.com>
2022-10-29 18:48:10 +0900

    Add page storing

--- frontend/src/routes/Detail.svelte
+++ frontend/src/routes/Detail.svelte
@@ -1,6 +1,7 @@
 <script>
     import fastapi from '../lib/api'
     import Error from "../components/Error.svelte"
+    import { push } from 'svelte-spa-router'
 
     export let params = {}
     let question_id = params.question_id
@@ -33,30 +34,6 @@
         )
     }
 </script>
-<!-- 
-<h1>{question.subject}</h1>
-<div>
-    {question.content}
-</div>
-<ul>
-    {#each question.answers as answer}
-        <li>{answer.content}</li>
-    {/each}
-</ul>
-<Error error={error} />
-<form method="post">
-    <textarea rows="15" bind:value={content}></textarea>
-    <input type="submit" value="답변등록" on:click="{post_answer}">
-</form>
-
-<style>
-    textarea {
-        width:100%;
-    }
-    input[type=submit] {
-        margin-top:10px;
-    }
-</style> -->
 
 <div class="container my-3">
     <!-- 질문 -->
@@ -71,6 +48,9 @@
             </div>
         </div>
     </div>
+
+    <button class="btn btn-secondary" on:click="{() => {push('/')}}">목록으로</button>
+
     <!-- 답변 목록 -->
     <h5 class="border-bottom my-3 py-2">{question.answers.length}개의 답변이 있습니다.</h5>
     {#each question.answers as answer}
--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -1,10 +1,10 @@
 <script>
     import fastapi from "../lib/api"
     import { link } from 'svelte-spa-router'
+    import { page } from "../lib/store"
 
     let question_list = []
     let size = 10
-    let page = 0
     let total = 0
     $: total_page = Math.ceil(total/size)
   
@@ -15,19 +15,13 @@
         }
         fastapi('get', '/api/question/list', params, (json) => {
             question_list = json.question_list
-            page = _page
+            $page = _page
             total = json.total
         })
     }
 
-    get_question_list(0)
+    $: get_question_list($page)
 </script>
-  
-<!-- <ul>
-    {#each question_list as question}
-        <li><a use:link href="/detail/{question.id}">{question.subject}</a></li>
-    {/each}
-</ul> -->
 
 <div class="container my-3">
     <table class="table">
@@ -53,34 +47,34 @@
     <!-- 페이징 처리 시작 -->
     <ul class="pagination justify-content-center">
         <!-- 이전 페이지 -->
-        <li class="page-item {page <= 0 && 'disabled'}">
-            <button class="page-link" on:click="{() => get_question_list(page-1)}">이전</button>
+        <li class="page-item {$page <= 0 && 'disabled'}">
+            <button class="page-link" on:click="{() => get_question_list($page-1)}">이전</button>
         </li>
         <!-- 페이지 번호 -->
         {#each Array(total_page) as _, loop_page}
         <!-- 첫번째 페이지는 강제로 표시 -->
         {#if loop_page === 0}
-            <li class="page-item {loop_page === page && 'active'}">
+            <li class="page-item {loop_page === $page && 'active'}">
                 <button class="page-link" on:click="{() => get_question_list(loop_page)}">{loop_page+1}</button>
             </li>
         <!-- 마지막 페이지도 강제로 표시 -->
         {:else if loop_page === total_page-1}
-            <li class="page-item {loop_page === page && 'active'}">
+            <li class="page-item {loop_page === $page && 'active'}">
                 <button class="page-link" on:click="{() => get_question_list(loop_page)}">{loop_page+1}</button>
             </li>
         <!-- 첫번째/마지막 페이지가 아닌 경우, 현재 페이지 전후 3개 페이지를 표시 -->
-        {:else if loop_page > 0 && loop_page >= page-3 && loop_page <= page+3 && loop_page < total_page-1}
+        {:else if loop_page > 0 && loop_page >= $page-3 && loop_page <= $page+3 && loop_page < total_page-1}
             <!-- 현재 페이지가 5번 이상인 경우 1번째 페이지와 현재페이지-3 페이지 사이에 ... 등록 -->
-            {#if loop_page === page-3 && loop_page > 1}
+            {#if loop_page === $page-3 && loop_page > 1}
             <li class="page-item {'disabled'}">
                 <button class="page-link">...</button>
             </li>
             {/if}
-            <li class="page-item {loop_page === page && 'active'}">
+            <li class="page-item {loop_page === $page && 'active'}">
                 <button class="page-link" on:click="{() => get_question_list(loop_page)}">{loop_page+1}</button>
             </li>
             <!-- 마찬가지로 마지막 페이지와 현재 페이지+3 페이지 사이에 ... 입력 -->
-            {#if loop_page === page+3 && loop_page < total_page-2}
+            {#if loop_page === $page+3 && loop_page < total_page-2}
             <li class="page-item {'disabled'}">
                 <button class="page-link">...</button>
             </li>
@@ -88,8 +82,8 @@
         {/if}
         {/each}
         <!-- 다음 페이지 -->
-        <li class="page-item {page >= total_page-1 && 'disabled'}">
-            <button class="page-link" on:click="{() => get_question_list(page+1)}">다음</button>
+        <li class="page-item {$page >= total_page-1 && 'disabled'}">
+            <button class="page-link" on:click="{() => get_question_list($page+1)}">다음</button>
         </li>
     </ul>
     <!-- 페이징 처리 끝 -->
