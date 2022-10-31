7ceb02d998f38baffee148af5b2772d4c9f469c3
Inhwan Lee <lih0905@gmail.com>
2022-10-31 21:51:46 +0900

    Require login to post a question/answer

--- models.py
+++ models.py
@@ -10,6 +10,8 @@ class Question(Base):
     subject = Column(String, nullable=False)
     content = Column(Text, nullable=False)
     create_date = Column(DateTime, nullable=False)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=True)
+    user = relationship("User", backref="question_users")
 
 class Answer(Base):
     __tablename__ = "answer"
@@ -19,6 +21,8 @@ class Answer(Base):
     create_date = Column(DateTime, nullable=False)
     question_id = Column(Integer, ForeignKey("question.id"))
     question = relationship("Question", backref="answers")
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=True)
+    user = relationship("User", backref="answer_users")
 
 class User(Base):
     __tablename__ = "user"
