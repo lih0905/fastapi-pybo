b6f148f5691aef6caeed283910229352495366eb
Inhwan Lee <lih0905@gmail.com>
2022-11-05 19:44:09 +0900

    Add search feature

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -1,31 +1,47 @@
 <script>
     import fastapi from "../lib/api"
     import { link } from 'svelte-spa-router'
-    import { page, is_login } from "../lib/store"
+    import { page, keyword, is_login } from "../lib/store"
     import moment from 'moment/min/moment-with-locales'
     moment.locale('ko')
 
     let question_list = []
     let size = 10
     let total = 0
+    let kw = ''
     $: total_page = Math.ceil(total/size)
   
-    function get_question_list(_page) {
+    function get_question_list() {
         let params = {
-            page: _page,
+            page: $page,
             size: size,
+            keyword: $keyword,
         }
         fastapi('get', '/api/question/list', params, (json) => {
             question_list = json.question_list
-            $page = _page
             total = json.total
+            kw = $keyword
         })
     }
 
-    $: get_question_list($page)
+    $: $page, $keyword, get_question_list()
 </script>
 
 <div class="container my-3">
+    <div class="row my-3">
+        <div class="col-6">
+            <a user:link href="/question-create" 
+                class="btn btn-primary {$is_login ? '' : 'disabled'}">질문 등록하기</a>
+        </div>
+        <div class="col-6">
+            <div class="input-group">
+                <input type="text" class="form-control" bind:value="{kw}">
+                <button class="btn btn-outlin-secondary" on:click={() => {$keyword = kw, $page = 0}}>
+                    찾기
+                </button>
+            </div>
+        </div>
+    </div>
     <table class="table">
         <thead>
             <tr class="text-center table-dark">
@@ -55,19 +71,19 @@
     <ul class="pagination justify-content-center">
         <!-- 이전 페이지 -->
         <li class="page-item {$page <= 0 && 'disabled'}">
-            <button class="page-link" on:click="{() => get_question_list($page-1)}">이전</button>
+            <button class="page-link" on:click="{() => $page--}">이전</button>
         </li>
         <!-- 페이지 번호 -->
         {#each Array(total_page) as _, loop_page}
         <!-- 첫번째 페이지는 강제로 표시 -->
         {#if loop_page === 0}
             <li class="page-item {loop_page === $page && 'active'}">
-                <button class="page-link" on:click="{() => get_question_list(loop_page)}">{loop_page+1}</button>
+                <button class="page-link" on:click="{() => $page = loop_page}">{loop_page+1}</button>
             </li>
         <!-- 마지막 페이지도 강제로 표시 -->
         {:else if loop_page === total_page-1}
             <li class="page-item {loop_page === $page && 'active'}">
-                <button class="page-link" on:click="{() => get_question_list(loop_page)}">{loop_page+1}</button>
+                <button class="page-link" on:click="{() => $page = loop_page}">{loop_page+1}</button>
             </li>
         <!-- 첫번째/마지막 페이지가 아닌 경우, 현재 페이지 전후 3개 페이지를 표시 -->
         {:else if loop_page > 0 && loop_page >= $page-3 && loop_page <= $page+3 && loop_page < total_page-1}
@@ -78,7 +94,7 @@
             </li>
             {/if}
             <li class="page-item {loop_page === $page && 'active'}">
-                <button class="page-link" on:click="{() => get_question_list(loop_page)}">{loop_page+1}</button>
+                <button class="page-link" on:click={() => {$page = loop_page}}>{loop_page+1}</button>
             </li>
             <!-- 마찬가지로 마지막 페이지와 현재 페이지+3 페이지 사이에 ... 입력 -->
             {#if loop_page === $page+3 && loop_page < total_page-2}
@@ -90,10 +106,8 @@
         {/each}
         <!-- 다음 페이지 -->
         <li class="page-item {$page >= total_page-1 && 'disabled'}">
-            <button class="page-link" on:click="{() => get_question_list($page+1)}">다음</button>
+            <button class="page-link" on:click="{() => $page++}">다음</button>
         </li>
     </ul>
     <!-- 페이징 처리 끝 -->
-    <a use:link href="/question-create" 
-        class="btn btn-primary {$is_login ? '' : 'disabled'}">질문 등록하기</a>
 </div>
